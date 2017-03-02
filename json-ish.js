ecb87f7d19da659611b8ab6d4536131c8dd96371
Sam Goto <samuelgoto@gmail.com>
2017-03-01 16:52:35 -0800

    creating a framework to develop the code under unittests

--- json-ish.js
+++ json-ish.js
@@ -69,3 +69,65 @@ Call ${opt_parent}() to dereference it.
   }
   return node;
 }
+
+
+function Submitter(form, data, opt_fetcher) {
+  this.form = form;
+  this.data = data;
+  this.fetcher = opt_fetcher || window;
+}
+
+Submitter.prototype.submit = function() {
+  // returns a promise?
+  var formData = new FormData();
+
+  new InputWalker(this.form).forEach((input, prop) => {
+    // Populates all of the fields wiht the data points.
+
+    input["@name"] = input["@name"] || prop;
+    var name = input["@name"];
+
+    if (this.data[name]) {
+      // TODO(goto): make sure that the value matches the input type
+      // as well as the type constrains (e.g. min, max, etc).
+      input["@value"] = this.data[name];
+    }
+  }).forEach(input => {
+    // Checks if all of the required fields are present.
+    if (input["@required"] && !input["@value"]) {
+      throw new Error("required field isn't set: " + input["@name"]);
+    }
+  }).forEach(input => {
+    // console.log(input);
+    formData.append(input["@name"], input["@value"]);
+    // console.log(formData);
+  });
+
+  return this.fetcher
+      .fetch(this.form["@action"], {
+	method: this.form["@method"] || "GET",
+	body: formData
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not OK: " + response);
+        }
+	  return response.json();
+        })
+      .then(result => $(result));
+}
+
+function InputWalker(form) {
+  this.form = form;
+}
+
+InputWalker.prototype.forEach = function(callback) {
+  for (var prop in this.form) {
+    if (prop[0] == "@") {
+      continue;
+    }
+
+    callback(this.form[prop], prop);
+  }
+  return this;
+}
